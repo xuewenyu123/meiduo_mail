afec80b9c6e4dc8a6980498728ec74c76afcd427
xuewenyu123 <3515281812@qq.com>
2023-03-02 14:45:02 +0800

    配置log记录；设置通过域名访问

--- meiduo_mall/settings.py
+++ meiduo_mall/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vbd+a)@ckc8y03&t1+$9)tk+a%hv9qw^g#1&x0+&m0*#whq7=3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.meiduomall.site', '127.0.0.1']
 
 
 # Application definition
@@ -143,4 +143,46 @@ CACHES = {
 }
 # 将session数据保存到缓存中
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
+SESSION_CACHE_ALIAS = "session"
+
+#################日志####################
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,  # 是否禁用已经存在的日志器
+    'formatters': {  # 日志信息显示格式
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
+        }
+    },
+    'filters': {  # 对日志进行过滤
+        'require_debug_true': {  # django在debug模式下才输出日志
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {  # 日志处理方法
+        'console': {  # 向终端中输出日志
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {  # 向文件中输出日志
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/meiduo.log'),  # 日志文件位置
+            'maxBytes': 300 * 1024 * 1024,  # 每个文件的最大内存
+            'backupCount': 10,  # 备份
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {  # 日志器
+        'django': {  # 定义了一个名为django的日志器
+            'handlers': ['console', 'file'] ,  # 可以同时向终端与文件中输出日志
+            'propagete': True,
+            'level': 'INFO',
+        }
+    }
+}
