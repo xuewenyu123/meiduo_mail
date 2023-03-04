f345ae6c8d61d82d75d30a039d0c8527df43f3a3
xuewenyu123 <3515281812@qq.com>
2023-03-04 18:52:48 +0800

    状态保持的实现

--- apps/users/views.py
+++ apps/users/views.py
@@ -69,43 +69,59 @@ class UsernameCountView(View):
 import json
 
 class RegisterView(View):
-     
-     def post(self, request):
-          # 1. 接收请求(POST json)
-          body_bytes = request.body
-          body_str = body_bytes.decode()
-          body_dict = json.loads(body_str)
-          # 2. 获取数据
-          username = body_dict.get("username")
-          password = body_dict.get('password')
-          password2 = body_dict.get('password2')
-          mobile = body_dict.get('moblie')
-          allow = body_dict.get('allow')
-          # 3. 验证数据
-          # 3.1 用户名、密码、确认密码、手机号、验证码、是否同意协议 都要有
-          if not all([username, password, password2, mobile]):
-              return JsonResponse({"code": 400, "errmsg": "参数不全"}) 
-          # 3.2 验证用户名不可以重复
-          if not re.match('[a-zA-Z0-9_-]{5,20}', username):
-              return JsonResponse({"code": 400, "errmsg": "用户名不符合规则"})
-          # 3.3 验证密码满足规则
-          # 3.4 确认密码和密码要一致
-       #    if password != password2:
-       #         return JsonResponse({"code": 400, "errmsg": "密码不一致"})
-       #    # 3.5 手机号要满足规则 手机号不能重复
-       #    obj = User.objects.filter(moblie=mobile)
-       #    if obj:
-       #         return JsonResponse({"code": 400, "errmsg": "该手机号已被注册"})
-       #    # 3.6 需要同意协议
-       #    if not allow:
-       #         return JsonResponse({"code": 400, "errmsg": "请同意协议"})
-          # 4. 数据入库
-          # 方式一
-       #    user = User(username=username, password=password, moblie=moblie)
-       #    user.save()
-          # 方式二
-       #    User.objects.create(us1       qwername=username, password=password, moblie=moblie)
-          # 密码没有加密的解决方法
-          User.objects.create_user(username=username, password=password, moblie=mobile)
-          # 5. 返回响应
-          return JsonResponse({"code": 0, "errmsg": "ok"})
+      def post(self, request):
+              # 1. 接收请求(POST json)
+              body_bytes = request.body
+              body_str = body_bytes.decode()
+              body_dict = json.loads(body_str)
+              # 2. 获取数据
+              username = body_dict.get("username")
+              password = body_dict.get('password')
+              password2 = body_dict.get('password2')
+              mobile = body_dict.get('moblie')
+              allow = body_dict.get('allow')
+              # 3. 验证数据
+              # 3.1 用户名、密码、确认密码、手机号、验证码、是否同意协议 都要有
+              if not all([username, password, password2, mobile]):
+                     return JsonResponse({"code": 400, "errmsg": "参数不全"}) 
+              # 3.2 验证用户名不可以重复
+              if not re.match('[a-zA-Z0-9_-]{5,20}', username):
+                     return JsonResponse({"code": 400, "errmsg": "用户名不符合规则"})
+              # 3.3 验证密码满足规则
+              # 3.4 确认密码和密码要一致
+              # if password != password2:
+              #    return JsonResponse({"code": 400, "errmsg": "密码不一致"})
+              # 3.5 手机号要满足规则 手机号不能重复
+              # obj = User.objects.filter(moblie=mobile)
+              # if obj:
+              #   return JsonResponse({"code": 400, "errmsg": "该手机号已被注册"})
+              # 3.6 需要同意协议
+              # if not allow:
+              #   return JsonResponse({"code": 400, "errmsg": "请同意协议"})
+              # 4. 数据入库
+              # 方式一
+              # user = User(username=username, password=password, moblie=mobile)
+              # user.save()
+              # 方式二
+              # User.objects.create(username=username, password=password, moblie=moblie)
+              # 密码没有加密的解决方法
+              user = User.objects.create_user(username=username, password=password, moblie=mobile)
+
+              # 如何设置session
+              # request.session["user_id"] = user_id
+              # django 实现的状态保持
+              from django.contrib.auth import login
+              login(request, user)
+
+              # 5. 返回响应
+              return JsonResponse({"code": 0, "errmsg": "ok"})
+
+
+
+"""
+1.注册成功之后自动登录
+2.注册成功之后弹出登录页面，用户还需在登录一下
+
+实现状态保持：
+1.cookie   2.session
+"""
